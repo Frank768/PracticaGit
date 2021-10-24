b8dd806dcca5a7243abe382d505b4193bb2de55c
Frank <frankelec@gmail.com>
2021-10-23 20:00:19 -0500

    Versión 5 de experimentos

--- calcular.js
+++ calcular.js
@@ -4,7 +4,7 @@ function devolverResultado (x,y)
 {
     var resultado = x+y;
     var result = resultado + (100/50);
-    console.log ("imprimir desde master :"+result);
+    console.log ("imprimir desde experimentos :"+result);
     return result;
 }
 x=10;
