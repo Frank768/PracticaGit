b457823a0c08aaa9e14baddaae9335acc5dfbf43
Frank <frankelec@gmail.com>
2021-10-23 20:44:47 -0500

    Se soluciona conflicto al fusionar las ramas

--- calcular.js
+++ calcular.js
@@ -4,11 +4,7 @@ function devolverResultado (x,y)
 {
     var resultado = x+y;
     var result = resultado + (100/50);
-<<<<<<< HEAD
     console.log ("imprimir desde experimentos pasando por master:"+result);
-=======
-    console.log ("imprimir desde experimentos sin pasar por master :"+result);
->>>>>>> experimentos
     return result;
 }
 x=10;
