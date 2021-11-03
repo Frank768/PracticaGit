b9e92a48e3f46878fd85d8114231f41e3d918366
Frank768 <frankelec@gmail.com>
2021-11-02 20:41:49 -0500

    Tildes y typos arreglados en calcular.js

--- calcular.js
+++ calcular.js
@@ -12,6 +12,6 @@ function devolverResultado (x,y)
 x=10;
 y=20;
 devolverResultado (x,y);
-alert ("fin del codigo, en espera de neuva instrucción");
+alert ("fin del código, en espera de nueva instrucción");
 //Resultado a mostar en pantalla
 //Se agrega comentario al footer
